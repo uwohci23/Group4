6c420a2ebdbe4d1f8b6ca0af05f205bfbb96d9a0
Jana Elshantaly <jelshant@uwo.ca>
2023-04-01 23:03:25 -0400

    signup fs module not working

--- src/login_screen.js
+++ src/login_screen.js
@@ -16,6 +16,29 @@ document.getElementById('login-form').addEventListener('submit', event => {
     alert('Inavlid username or password');
   }
 });
+document.getElementById('signup-form').addEventListener('submit', event => {
+    event.preventDefault();
+
+    const user_name = document.getElementById('input-user').value;
+    const password = document.getElementById('input-password').value;
+
+    const newUser = {
+        user_name: user_name,
+        password: password
+    }
+    users.push(newUser);
+    const data = JSON.stringify(users);
+    const fs = require('fs');
+    fs.writeFile('./users.json', data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('users file updated');
+    });
+    window.location.replace("./main_menu.html");
+    event.target.reset();
+});
 
 const loginForm = document.getElementById('login-form');
 const signupForm = document.getElementById('signup-form');
